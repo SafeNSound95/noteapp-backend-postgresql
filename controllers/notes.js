315356d1dabca009f2be7bf80ab8e45fd51a0822
Saif Shalan <safensound95@gmail.com>
2026-06-05 19:17:19 +0300

    feat: Implement admin role and ability to disable users accounts

--- controllers/notes.js
+++ controllers/notes.js
@@ -1,10 +1,8 @@
 const router = require("express").Router();
 
-const jwt = require("jsonwebtoken");
-const { SECRET } = require("../util/config");
-
 const { Note } = require("../models");
 const { User } = require("../models");
+const { tokenExtractor } = require("../util/middleware");
 const { Op } = require("sequelize");
 
 router.get("/", async (req, res) => {
@@ -31,20 +29,6 @@ router.get("/", async (req, res) => {
 
   res.json(notes);
 });
-
-const tokenExtractor = (req, res, next) => {
-  const authorization = req.get("authorization");
-  if (authorization && authorization.toLowerCase().startsWith("bearer ")) {
-    try {
-      req.decodedToken = jwt.verify(authorization.substring(7), SECRET);
-    } catch {
-      return res.status(401).json({ error: "token invalid" });
-    }
-  } else {
-    return res.status(401).json({ error: "token missing" });
-  }
-  next();
-};
 
 router.post("/", tokenExtractor, async (req, res) => {
   try {
--- controllers/users.js
+++ controllers/users.js
@@ -2,6 +2,8 @@ const router = require("express").Router();
 
 const { User } = require("../models");
 const { Note } = require("../models");
+const { tokenExtractor } = require("../util/middleware");
+const { isAdmin } = require("../util/middleware");
 
 router.get("/", async (req, res) => {
   const users = await User.findAll({
@@ -27,6 +29,22 @@ router.post("/", async (req, res) => {
 router.get("/:id", async (req, res) => {
   const user = await User.findByPk(req.params.id);
   if (user) {
+    res.json(user);
+  } else {
+    res.status(404).end();
+  }
+});
+
+router.put("/:username", tokenExtractor, isAdmin, async (req, res) => {
+  const user = await User.findOne({
+    where: {
+      username: req.params.username,
+    },
+  });
+
+  if (user) {
+    user.disabled = req.body.disabled;
+    await user.save();
     res.json(user);
   } else {
     res.status(404).end();
